4a63f9ab4e7215c9fa72ee6447960fc9cde2f620
satilpereira <satil.prog@gmail.com>
2023-05-24 19:37:09 -0300

    wip

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import '@styles/globals.css';
+
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover and share AI prompts',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
